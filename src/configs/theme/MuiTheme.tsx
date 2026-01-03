eb50a59c817d1360cc9abb13d59e5392192aa96f
pooulad <pooulad@gmail.com>
2026-01-03 02:23:47 -0500

    make font better

--- src/configs/theme/MuiTheme.tsx
+++ src/configs/theme/MuiTheme.tsx
@@ -2,7 +2,7 @@ import { createContext, useMemo, useState } from "react";
 import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import createCache from "@emotion/cache";
 import rtlPlugin from "stylis-plugin-rtl";
-import Vazirmatn from "../../assets/fonts/Vazirmatn.ttf";
+import Lalezar from "../../assets/fonts/Lalezar-Regular.ttf";
 import Orbitron from "../../assets/fonts/Orbitron.ttf";
 
 declare module "@mui/material/styles" {
@@ -28,6 +28,7 @@ declare module "@mui/material/TextField" {
     fourthary: true;
   }
 }
+
 interface Props {
   children: React.ReactNode;
 }
@@ -35,7 +36,7 @@ interface Props {
 type themeColor = "light" | "dark";
 
 export const ColorModeContext = createContext({
-  toggleColorMode: () => {},
+  toggleColorMode: () => { },
 });
 
 export const cacheRtl = createCache({
@@ -60,130 +61,129 @@ const Theme: React.FC<Props> = ({ children }) => {
     }),
     []
   );
-  localStorage.setItem("theme", mode);
+  useMemo(() => {
+    localStorage.setItem("theme", mode);
+  }, [mode]);
   const theme = useMemo(
     () =>
       createTheme({
         direction: "rtl",
         typography: {
-          fontFamily: ["Vazirmatn", "Orbitron"].join(","),
+          fontFamily: '"Lalezar", "Orbitron"',
         },
         components: {
           MuiCssBaseline: {
-            styleOverrides: {
-              "@font-face": [
-                {
-                  fontFamily: "Vazirmatn",
-                  fontStyle: "normal",
-                  fontDisplay: "swap",
-                  fontWeight: 400,
-                  src: `url(${Vazirmatn}) format("truetype")`,
-                  unicodeRange:
-                    "U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF",
-                },
-                {
-                  fontFamily: "Orbitron",
-                  fontStyle: "normal",
-                  fontDisplay: "swap",
-                  fontWeight: 400,
-                  src: `url(${Orbitron}) format("truetype")`,
-                  unicodeRange:
-                    "U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF",
-                },
-              ],
-              body: {
-                a: {
-                  textDecoration: "none",
-                  color: "inherit",
-                },
-                "::-webkit-scrollbar": {
-                  borderRadius: "10px",
-                  width: "6px",
-                },
-                "::-webkit-scrollbar-track": {
-                  borderRadius: "10px",
-                  backdropFilter: "blur(4px)",
-                },
-                "::-webkit-scrollbar-thumb": {
-                  borderRadius: "10px",
-                  backgroundColor: "#33691E",
-                },
-              },
-            },
+            styleOverrides: `
+              @font-face {
+                font-family: 'Lalezar';
+                font-style: normal;
+                font-display: swap;
+                font-weight: 400;
+                src: url(${Lalezar}) format('truetype');
+                unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
+              }
+              @font-face {
+                font-family: 'Orbitron';
+                font-style: normal;
+                font-display: swap;
+                font-weight: 400;
+                src: url(${Orbitron}) format('truetype');
+                unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
+              }
+              body {
+                font-family: 'Lalezar', sans-serif;
+                a {
+                  text-decoration: none;
+                  color: inherit;
+                }
+                '::-webkit-scrollbar' {
+                  border-radius: 10px;
+                  width: 6px;
+                }
+                '::-webkit-scrollbar-track' {
+                  border-radius: 10px;
+                  backdrop-filter: blur(4px);
+                }
+                '::-webkit-scrollbar-thumb' {
+                  border-radius: 10px;
+                  background-color: #33691E;
+                }
+              }
+            `,
           },
         },
         palette: {
           mode,
           ...(mode === "light"
             ? {
-                primary: {
-                  main: "#7CB342",
-                  contrastText: "#ffffff",
-                },
-                secondary: {
-                  main: "#33691E",
-                },
-                thirdary: {
-                  main: "#558B2F",
-                  contrastText: "#ffffff",
-                },
-                fourthary: {
-                  main: "#AED581",
-                },
-                success: {
-                  main: "#43A047",
-                },
-                error: {
-                  main: "#E53935",
-                },
-                background: {
-                  paper: "#FAFAFA",
-                  default: "#FFFFFF",
-                },
-                action: {
-                  disabled: "#DDE8D1",
-                },
-                text: {
-                  primary: "#000000",
-                  secondary: "#181616ff",
-                  disabled: "#B0BEC5",
-                },
-                divider: "#C5E1A5",
-              }
+              primary: {
+                main: "#7CB342",
+                contrastText: "#ffffff",
+              },
+              secondary: {
+                main: "#33691E",
+              },
+              thirdary: {
+                main: "#558B2F",
+                contrastText: "#ffffff",
+              },
+              fourthary: {
+                main: "#AED581",
+              },
+              success: {
+                main: "#43A047",
+              },
+              error: {
+                main: "#E53935",
+              },
+              background: {
+                paper: "#FAFAFA",
+                default: "#FFFFFF",
+              },
+              action: {
+                disabled: "#DDE8D1",
+              },
+              text: {
+                primary: "#000000",
+                secondary: "#181616ff",
+                disabled: "#B0BEC5",
+              },
+              divider: "#C5E1A5",
+            }
             : {
-                primary: {
-                  main: "#7CB342",
-                  contrastText: "#000000",
-                },
-                secondary: {
-                  main: "#C5E1A5",
-                },
-                thirdary: {
-                  main: "#9CCC65",
-                },
-                fourthary: {
-                  main: "#33691E",
-                },
-                success: {
-                  main: "#66BB6A",
-                },
-                error: {
-                  main: "#EF5350",
-                },
-                background: {
-                  paper: "#1B1B1B",
-                  default: "#121212",
-                },
-                action: {
-                  disabled: "#445A2B",
-                },
-                text: {
-                  primary: "#E0F2F1",
-                  secondary: "#B2DFDB",
-                  disabled: "#789262",
-                },
-                divider: "#4CAF50",
-              }),
+              primary: {
+                main: "#7CB342",
+                contrastText: "#000000",
+              },
+              secondary: {
+                main: "#C5E1A5",
+              },
+              thirdary: {
+                main: "#9CCC65",
+              },
+              fourthary: {
+                main: "#33691E",
+              },
+              success: {
+                main: "#66BB6A",
+              },
+              error: {
+                main: "#EF5350",
+              },
+              background: {
+                paper: "#1B1B1B",
+                default: "#121212",
+              },
+              action: {
+                disabled: "#445A2B",
+              },
+              text: {
+                primary: "#E0F2F1",
+                secondary: "#B2DFDB",
+                disabled: "#789262",
+              },
+              divider: "#4CAF50",
+            }),
         },
       }),
     [mode]
